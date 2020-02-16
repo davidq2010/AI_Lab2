d636b569ecc2c8cb1963b843b9da96f8cf8598a7
davidq2010 <david.qin@richmond.edu>
2020-02-16 03:29:13 -0500

    Base class for Pieces (ideas only, no implem)

--- src/ChessAI.java
+++ src/ChessAI.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class Chess {
+public class ChessAI {
 
     public static ArrayList<ArrayList<String>> GetBoard(String fileName)
         throws IOException {
---
+++ src/Piece.java
@@ -0,0 +1,11 @@
+
+public class Piece {
+    // Parent class of all the pieces, which will implement a move() method.
+    // Ideas for private members:
+    //  Movable squares
+    //
+
+    public Piece() {
+
+    }
+}
